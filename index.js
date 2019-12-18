0e6375baf41967ef18cf1e498b05f1b68f688542
Cyber Ludthra <rludthra@gmail.com>
2019-12-18 23:08:36 +0800

    nigga nuts

--- index.js
+++ index.js
@@ -113,83 +113,10 @@ client.on('message', function(message) {
 client.on('message', message => {
 	// Detection Commands
 	if(message.author.bot) return;
-	const commandcase = message.content.toLowerCase();
-	const pingmentionedmember = message.mentions.users.first();
-	const isGay = [
-		'It the truth though :pensive: :pensive: :pensive:',
-		pingmentionedmember + 'is gay asf tho.',
-		'You\'re not wrong... :pensive:',
-	];
-
-	// "lmao Detection Bully" Command
-	if (commandcase.includes('lmao') || commandcase.includes('l m a o') || commandcase.includes('lmfao') || commandcase.includes('l m f a o')) {
-		message.channel.send('Did you lose your ass again? Let me get it for you.');
-	}
-	// "true true Detection Bully" Command
-	if (commandcase.includes('true true') || commandcase.includes('t r u e  t r u e')) {
-		message.channel.send('true true', {
-			file: 'http://media.discordapp.net/attachments/374885445030510592/567738823191035924/JPEG_20190404_175259.jpg',
-		});
-	}
-	// "wasted Detection Bully" Command
-	if (commandcase.includes('wasted') || commandcase.includes('w a s t e d')) {
-		message.channel.send('', {
-			file: 'https://cdn.discordapp.com/attachments/570537687954751528/570544623827550208/hqdefault.png',
-		});
-	}
-	// "🦀 Detection Bully" Command
-	if (commandcase.includes('is gone')) {
-		message.channel.send('', {
-			file: 'https://cdn.discordapp.com/attachments/374885445030510592/570556768707674132/tenor.gif',
-		});
-	}
-	// "clowning Detection Bully" Command
-	if (commandcase.includes('is gay') & commandcase.includes('<@') & commandcase.includes('>') != commandcase.includes('!567328033254408209') || commandcase.includes('be gay') & commandcase.includes('<@') & commandcase.includes('>') != commandcase.includes('!567328033254408209') || commandcase.includes('gay') & commandcase.includes('<@') & commandcase.includes('>') != commandcase.includes('!567328033254408209')) {
-		message.channel.send(isGay[Math.floor(Math.random() * isGay.length)]);
-	}
-	// "TITI Detection Bully" Command
-	if (commandcase.includes('titi')) {
-		message.channel.send('', {
-			file: 'https://cdn.discordapp.com/attachments/570537687954751528/570557477750439936/JPEG_20190424_134841.jpg',
-		});
-	}
-	// "bruh Detection Bully" Command
-	if (commandcase.includes('bruh') || commandcase.includes('b r u h') || commandcase.includes('breh') || commandcase.includes('b r e h')) {
-		message.channel.send(' ', {
-			file: 'https://pbs.twimg.com/profile_images/1066371730074144769/dXKx9agF_400x400.jpg',
-		});
-	}
-
 	if(message.author.bot) return;
 	const args = message.content.trim().split(/ +/g);
 	if((args[0].toLowerCase() == 'i\'m' || args[0].toLowerCase() == 'im' || args[0].toLowerCase() == 'am') && (args[1]) && !client.disabledMembers.has(message.author.id)) {
 		message.channel.send(`Hi ${args.slice(1).join(' ')}, I'm dad`);
-	}
-	// "bruh Detection Bully" Command
-	if (commandcase.includes('rarted')) {
-		message.channel.send('Rawr x3 nuzzles how are you pounces on you you\'re so warm o3o notices you have a bulge o: someone\'s happy nuzzles your necky wecky~ murr hehehe rubbies your bulgy wolgy you\'re so big :oooo rubbies more on your bulgy wolgy it doesn\'t stop growing ·www· kisses you and lickies your necky daddy likies (; nuzzles wuzzles I hope daddy really likes $: wiggles butt and squirms I want to see your big daddy meat wiggles butt I have a little itch o3o wags tail can you please get my itch puts paws on your chest nyea its a seven inch itch rubs your chest can you help me pwease squirms pwetty pwease sad face I need to be punished runs paws down your chest and bites lip like I need to be punished really good paws on your bulge as I lick my lips I\'m getting thirsty. I can go for some milk unbuttons your pants as my eyes glow you smell so musky :v licks shaft* mmmm so musky drools all over your daddy meat I like fondles Mr. Fuzzy Balls hehe puts snout on balls and inhales deeply oh god im so hard licks balls punish me daddy nyea squirms more and wiggles butt I love your musky goodness bites lip please punish me licks lips* nyea suckles on your tip so good licks pre of your cock salty goodness eyes role back and goes balls deep mmmm moans and suckles', {
-		});
-	}
-	// "clowning Detection Bully" Command
-	if (commandcase.includes('clowning') & commandcase.includes('<@') & commandcase.includes('>') != commandcase.includes('!567328033254408209') || commandcase.includes('clowning') & commandcase.includes('<@') & commandcase.includes('>') != commandcase.includes('!567328033254408209')) {
-		message.channel.send(pingmentionedmember + ' out here clowning.', {
-			file: 'https://cdn.discordapp.com/attachments/391235516647079938/567982442053632000/clowning.jpg',
-		});
-	}
-	if (commandcase.includes('clowning') & commandcase.includes('<@') & commandcase.includes('>') & commandcase.includes('!567328033254408209')) {
-		message.channel.send(message.author + ' is the real clown here.', {
-			file: 'https://cdn.discordapp.com/attachments/391235516647079938/567982442053632000/clowning.jpg',
-		});
-	}
-	// "Fuck you BullyBot" Command
-	if (commandcase.includes('fuck you') & commandcase.includes('<@') & commandcase.includes('>')) {
-		message.channel.send('Nah fuck YOU, ' + message.author, {
-		});
-	}
-	// "@everyone Detection Bully" Command
-	if (commandcase.includes ('Supercali')) {
-		message.channel.send('Some Faggot: *Pings everyone on the fucking server.*');
-		message.channel.send('@everyone: :angry:');
 	}
 }
 );
